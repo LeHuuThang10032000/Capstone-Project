6c566b094847eb6b717ffbbe957aee7f5302459b
HangLoc <hangloc312001@gmail.com>
2022-11-08 22:53:43 +0700

    delete something

--- VLPay/src/screen/Home/Banner.tsx
+++ VLPay/src/screen/Home/Banner.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ImageBackground, TouchableWithoutFeedback, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ImageBackground, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 import TopUp from '../../assets/svg/topup.svg';
 import Withdraw from '../../assets/svg/withdraw.svg';
--- VLPay/src/screen/MyWallet/index.tsx
+++ VLPay/src/screen/MyWallet/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, Image, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
 import React from 'react';
 import { MainStackNavigation } from '../../stack/Navigation';
 import { useNavigation } from '@react-navigation/native';
