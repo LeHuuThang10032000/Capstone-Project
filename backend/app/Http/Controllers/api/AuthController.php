680bbd6a900a5f5ddac4f3490d830bf4aa7c4801
nguyen60c <nguyen60c@gmail.com>
2022-12-19 06:29:42 +0700

    function logout register

---
+++ backend/app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $field = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $field['name'],
+            'email' => $field['email'],
+            'password' => bcrypt($field['password'])
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' =>$token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request){
+        Auth::user()->tokens()->delete();
+
+        return [
+            'status' => 200
+        ];
+    }
+}
