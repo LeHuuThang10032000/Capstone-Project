df0672d2d96cce07d4ba999d59b7adc05d19abc0
nguyen60c <nguyen60c@gmail.com>
2022-12-27 19:28:52 +0700

    update

--- backend/app/Http/Controllers/api/AuthController.php
+++ backend/app/Http/Controllers/api/AuthController.php
@@ -12,13 +12,13 @@ class AuthController extends Controller
 {
     public function register(Request $request) {
         $field = $request->validate([
-            'full_name' => 'required|string',
+            'f_name' => 'required|string',
             'phone' => 'required|string|min:10|max:10|unique:users,phone',
             'password' => 'required|string|confirmed'
         ]);
 
         $user = User::create([
-            'f_name' => $field['full_name'],
+            'f_name' => $field['f_name'],
             'phone' => $field['phone'],
             'password' => bcrypt($field['password'])
         ]);
