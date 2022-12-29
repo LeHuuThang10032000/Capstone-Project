7a8e60986a0a00bb3506ea15e0815f1cfdc0dfde
hangloc31 <hangloc31@gmail.com>
2022-12-29 11:07:35 +0700

    update transaction history screen

--- VLPay/src/screen/TransactionHistory/index.tsx
+++ VLPay/src/screen/TransactionHistory/index.tsx
@@ -1,4 +1,4 @@
-import {ScrollView, StyleSheet} from 'react-native';
+import {FlatList, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
 import React from 'react';
 import HeaderComp from '../../components/HeaderComp';
 import {
@@ -10,133 +10,92 @@ import {
   Text,
   View,
   Divider,
+  Image,
 } from 'native-base';
 import SearchIcon from '../../assets/svg/search.svg';
 import HeaderDivider from '../Notification/HeaderDivider';
+import {HISTORY_TRANSACTION_SAMPLE} from './mock';
 
 const Index = () => {
   return (
-    <View style={styles.bgColor}>
-      <HeaderComp title="History" />
-      <Center bg="white">
-        <Input
-          placeholder="Search..."
-          fontFamily={'Poppins-Bold'}
-          width="90%"
-          borderRadius="4"
-          borderColor={'#A2A2A6'}
-          mt="21"
-          mb="21"
-          py="3"
-          px="1"
-          pl="2"
-          fontSize="16"
-          bgColor={'#C7CEEA'}
-          autoFocus={false}
-          // onChangeText={text => searchFilterFunction(text)}
-          // value={search}
-          InputLeftElement={<Icon m="2" ml="3" size="6" as={<SearchIcon />} />}
-        />
-      </Center>
-
-      <ScrollView>
-        <VStack space={4} alignItems="center" pt={5} pb={5}>
-          <Center w="90%" h={'10'} bg="#C7CEEA" alignItems="center">
-            <Text
-              style={{
-                fontFamily: 'Poppins-SemiBold',
-                fontSize: 16,
-                color: '#312E49',
-              }}>
-              Tháng 10/2022
-            </Text>
-          </Center>
-
-          <Center w="90%" h="10" bg="#ffffff" alignItems="flex-start">
-            <HStack justifyContent="space-between" w={'100%'} px="5">
-              <Text style={styles.titleText}>Chuyển tiền tới Huu Loc</Text>
-              <Text style={styles.titleText}>-1.000.000đ</Text>
-            </HStack>
-            <HStack justifyContent="space-between" w={'100%'} px="5">
-              <Text style={styles.text}>09:14 - 06/10/2022</Text>
-              <Text style={styles.text}>Thành công</Text>
-            </HStack>
-          </Center>
-
-          <Center w="90%" h="10" bg="#ffffff" alignItems="flex-start">
-            <HStack justifyContent="space-between" w={'100%'} px="5">
-              <Text style={styles.titleText}>Nhận tiền từ Thanh Tuan</Text>
-              <Text style={styles.titleText}>+1.000.000đ</Text>
-            </HStack>
-            <HStack justifyContent="space-between" w={'100%'} px="5">
-              <Text style={styles.text}>09:12 - 06/10/2022</Text>
-              <Text style={styles.text}>Thành công</Text>
-            </HStack>
-          </Center>
-
-          <Center w="90%" h={'10'} bg="#C7CEEA" alignItems="center">
-            <Text
-              style={{
-                fontFamily: 'Poppins-SemiBold',
-                fontSize: 16,
-                color: '#312E49',
-              }}>
-              Tháng 09/2022
-            </Text>
-          </Center>
-
-          <Center w="90%" h="10" bg="#ffffff" alignItems="flex-start">
-            <HStack justifyContent="space-between" w={'100%'} px="5">
-              <Text style={styles.titleText}>Chuyển tiền tới Robert Lam</Text>
-              <Text style={styles.titleText}>-10.000đ</Text>
-            </HStack>
-            <HStack justifyContent="space-between" w={'100%'} px="5">
-              <Text style={styles.text}>11:14 - 11/09/2022</Text>
-              <Text style={styles.text}>Thành công</Text>
-            </HStack>
-          </Center>
-
-          <Center w="90%" h="10" bg="#ffffff" alignItems="flex-start">
-            <HStack justifyContent="space-between" w={'100%'} px="5">
-              <Text style={styles.titleText}>Nhận tiền từ Jenny</Text>
-              <Text style={styles.titleText}>+100.000đ</Text>
-            </HStack>
-            <HStack justifyContent="space-between" w={'100%'} px="5">
-              <Text style={styles.text}>06:12 - 20/09/2022</Text>
-              <Text style={styles.text}>Thành công</Text>
-            </HStack>
-          </Center>
-
-          <Center w="90%" h="10" bg="#ffffff" alignItems="flex-start">
-            <HStack justifyContent="space-between" w={'100%'} px="5">
-              <Text style={styles.titleText}>Thanh toán hóa đơn</Text>
-              <Text style={styles.titleText}>-22.000.000đ</Text>
-            </HStack>
-            <HStack justifyContent="space-between" w={'100%'} px="5">
-              <Text style={styles.text}>06:14 - 20/09/2022</Text>
-              <Text style={styles.text}>Thành công</Text>
-            </HStack>
-          </Center>
-        </VStack>
-      </ScrollView>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <HeaderComp title="Lịch sử giao dịch" />
+      <View style={{paddingHorizontal: 15, flex: 1}}>
+        <Center bg="white">
+          <Input
+            placeholder="Tìm giao dịch"
+            fontFamily={'Poppins-Regular'}
+            width="100%"
+            borderRadius="4"
+            borderColor={'#A2A2A6'}
+            mt="21"
+            mb="21"
+            py="3"
+            px="1"
+            pl="2"
+            fontSize="16"
+            bgColor={'rgba(0, 0, 0, 0.06)'}
+            autoFocus={false}
+            // onChangeText={text => searchFilterFunction(text)}
+            // value={search}
+            InputLeftElement={
+              <Icon ml="3" as={<SearchIcon color={'#000000'} />} />
+            }
+          />
+        </Center>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {HISTORY_TRANSACTION_SAMPLE.map(item => {
+            return (
+              <View key={item.id}>
+                <View style={styles.containerMonth}>
+                  <Text style={styles.titleText}>{item.month}</Text>
+                </View>
+                {item.list_transaction.map(item => {
+                  return (
+                    <VStack py={5} key={item.id}>
+                      <HStack justifyContent={'space-between'}>
+                        <Text style={styles.text}>{item.description}</Text>
+                        <Text style={styles.text}>{item.money}đ</Text>
+                      </HStack>
+                      <Text style={styles.textDate}>{item.date}</Text>
+                    </VStack>
+                  );
+                })}
+                <Divider my={5} bgColor={'black'} />
+              </View>
+            );
+          })}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  bgColor: {
+  container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
+    // paddingHorizontal: 15,
   },
   text: {
     fontFamily: 'Poppins-SemiBold',
+    fontSize: 16,
+    color: '#312E49',
+  },
+  textDate: {
     fontSize: 14,
-    color: '#808080',
+    color: 'rgba(49, 46, 73, 0.5)',
+    fontWeight: '600',
   },
   titleText: {
     fontFamily: 'Poppins-Bold',
     color: '#312E49',
     fontSize: 16,
+  },
+  containerMonth: {
+    backgroundColor: '#C7CEEA4A',
+    alignItems: 'center',
+    padding: 20,
   },
 });
 
