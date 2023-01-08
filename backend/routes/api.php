ee49fa0a22e14c998eeeb6a13e931fc376f7d799
nguyen60c <nguyen60c@gmail.com>
2023-01-08 09:00:15 +0700

    update

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -33,7 +33,7 @@ Route::group(['middleware' => ['auth:api']], function(){
     Route::post('/updateProfile',[UserController::class, 'profileUpdate']);
     Route::get('/get-profile',[UserController::class, 'getProfile']);
     Route::post('/create-store', [UserController::class, 'createStoreRequest']);
-    Route::post('/find-user/{id}',[UserController::class, 'findUserById']);
+    Route::get('/find-user/{id}',[UserController::class, 'findUserById']);
 
     //Transaction
     Route::post('/create-transaction', [TransactionController::class, "transfer"]);
