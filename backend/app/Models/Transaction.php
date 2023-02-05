10a1f1af367e1bd4213039c73a5b120f5dc5cdba
nguyen60c <nguyen60c@gmail.com>
2023-02-05 15:29:56 +0700

    update

--- backend/app/Models/Transaction.php
+++ backend/app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Transaction extends Model
 {
@@ -12,16 +13,21 @@ class Transaction extends Model
     protected $table = 'transactions';
 
     protected $fillable = [
-        "from_id" ,
+        "from_id",
         "to_id",
         "amount",
         "message",
         "code",
         "title",
+        "created_at"
     ];
 
     protected $hidden = [
-        'created_at',
-        'updated_at',
+      "updated_at"
     ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->toDayDateTimeString();
+    }
 }
