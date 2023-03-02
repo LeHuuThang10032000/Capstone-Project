d69caaae7ca5c404d7532320298fd03c2dcec110
nguyen <nguyen60c@gmail.com>
2023-03-02 21:31:25 +0700

    update

--- backend/app/Http/Controllers/api/StoreController.php
+++ backend/app/Http/Controllers/api/StoreController.php
@@ -129,19 +129,23 @@ class StoreController extends Controller
 
             $newAddOns = [];
 
-            foreach($addOns as $key => $value) {
-                if(!$value->id){
-                    $addOnNew = AddOn::create(
-                        [
-                            'name' => $value->name,
-                            'price' => $value->price,
-                            'store_id' => $request->store_id,
-                        ]
-                    );
-                    $value->id =  $addOnNew->id;
-                    array_push($newAddOns, $addOnNew->id);
+            try{
+                foreach($addOns as $key => $value) {
+                    if(!$value->id){
+                        $addOnNew = AddOn::create(
+                            [
+                                'name' => $value->name,
+                                'price' => $value->price,
+                                'store_id' => $request->store_id,
+                            ]
+                        );
+                        $value->id =  $addOnNew->id;
+                        array_push($newAddOns, $addOnNew->id);
+                    }
+                    array_push($newAddOns, $value->id);
                 }
-                array_push($newAddOns, $value->id);
+            }catch(Exception $e){
+                return ApiResponse::failureResponse($e);
             }
 
             $product = new Product;
