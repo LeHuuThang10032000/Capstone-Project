021a5d5004eca5f12d7727d62bbb0e04cba2c4bb
hangloc31 <hangloc31@gmail.com>
2023-03-09 09:50:54 +0700

    fix error

--- VLPay/src/screen/Home/index.tsx
+++ VLPay/src/screen/Home/index.tsx
@@ -36,112 +36,6 @@ const Index = () => {
   const [credit, setCredit] = useState(0);
   const [isloading, setIsloading] = useState(false);
 
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [page, setPage] = useState<number>(1);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [refreshing, setRefreshing] = React.useState(false);
-  const [hasMore, setHasMore] = useState<boolean>(true);
-
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    fetchPosts();
-    setTimeout(() => {
-      setRefreshing(false);
-    }, 2000);
-  }, []);
-
-  console.log(posts);
-
-  const fetchPosts = async () => {
-    setIsLoading(true);
-    const response = await axiosClient.get(
-      //   `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`,
-      `https://zennoshop.cf/api/user/store?limit=10&page=${page}`,
-    );
-    const data = response.data?.data;
-    setPosts(posts.concat(data));
-    setIsLoading(false);
-    if (data.length === 0) {
-      setHasMore(false);
-    }
-  };
-
-  useEffect(() => {
-    if (hasMore) {
-      fetchPosts();
-    }
-  }, [page]);
-
-  const renderItem = ({item}: {item: Post}) => (
-    <TouchableOpacity
-      onPress={() =>
-        navigation.navigate('DetailStore', {
-          id: item.id,
-        })
-      }>
-      <HStack
-        py={3}
-        mx={3}
-        px={3}
-        mb={3}
-        backgroundColor="#FFFFFF"
-        alignItems="center"
-        borderWidth={1}
-        borderRadius={10}>
-        <Image
-          source={{uri: item.image}}
-          width={52}
-          height={52}
-          borderRadius={50}
-          borderWidth={1}
-          borderColor="#000000"
-          alt="img-store"
-        />
-        <VStack pl={3}>
-          <Heading size={'sm'}>
-            {item.name} ({item.location})
-          </Heading>
-          <Text>{item.phone}</Text>
-        </VStack>
-      </HStack>
-    </TouchableOpacity>
-  );
-
-  const renderFooter = () => {
-    if (isLoading && hasMore) {
-      return <ActivityIndicator />;
-    }
-    return null;
-  };
-
-  const renderHeader = () => {
-    return (
-      <>
-        <Header />
-        <Banner wallet={userWallet} />
-        <ManageCash wallet={userWallet} credit={credit} loading={isloading} />
-        <ContentWallet />
-        <PromoCarousel />
-        <Text
-          style={{
-            paddingHorizontal: 16,
-            paddingBottom: 16,
-            fontSize: 18,
-            fontFamily: 'Poppins-Bold',
-            color: '#000000',
-          }}>
-          Danh sách cửa hàng
-        </Text>
-      </>
-    );
-  };
-
-  const handleLoadMore = () => {
-    if (hasMore) {
-      setPage(page + 1);
-    }
-  };
-
   const fetchData = async () => {
     if (token) {
       setInterval(async () => {
@@ -165,16 +59,14 @@ const Index = () => {
   return (
     <>
       {token ? (
-        <FlatList
-          showsVerticalScrollIndicator={false}
-          data={posts}
-          keyExtractor={item => item.id.toString()}
-          renderItem={renderItem}
-          onEndReached={handleLoadMore}
-          onEndReachedThreshold={0.5}
-          ListFooterComponent={renderFooter}
-          ListHeaderComponent={renderHeader}
-        />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Header />
+          <Banner wallet={userWallet} />
+          <ManageCash wallet={userWallet} credit={credit} loading={isloading} />
+          <ContentWallet />
+          <PromoCarousel />
+          <ListStore />
+        </ScrollView>
       ) : (
         <Login />
       )}
