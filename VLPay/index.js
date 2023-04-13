9e308b041215d770df5b7605c600dab4a2f047ce
ngyen69c <nguyen69c>
2023-04-13 21:45:35 +0700

    upate

--- VLPay/index.js
+++ VLPay/index.js
@@ -5,5 +5,45 @@ import 'react-native-reanimated';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import PushNotification from 'react-native-push-notification';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import messaging from '@react-native-firebase/messaging';
+PushNotification.configure({
+  // (required) Called when a remote is received or opened, or local notification is opened
+  onNotification: function (notification) {
+    // process the notification
+    // (required) Called when a remote is received or opened, or local notification is opened
+    notification.finish(PushNotificationIOS.FetchResult.NewData);
+  },
+  // IOS ONLY (optional): default: all - Permissions to register.
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  requestPermissions: true,
+});
 
+async function registerAppWithFCM() {
+  await messaging().registerDeviceForRemoteMessages();
+}
+
+messaging().onMessage(async remoteMessage => {
+  console.log('Received a new notification', remoteMessage);
+});
+
+async function getDeviceToken() {
+  const deviceToken = await messaging().getToken();
+  console.log('Device Token:', deviceToken);
+}
+
+getDeviceToken();
+
+registerAppWithFCM();
+
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 AppRegistry.registerComponent(appName, () => App);
